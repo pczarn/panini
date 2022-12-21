9d4f67a26c1b07e458adda6ec044b61b8ec76de6
Piotr Czarnecki <pioczarn@gmail.com>
2022-12-21 08:59:14 +0100

    Cleanup logic_salsa

--- panini_logic_salsa/src/graph.rs
+++ panini_logic_salsa/src/graph.rs
@@ -50,6 +50,93 @@ pub type TyId = u32;
 pub type BindId = u32;
 pub type FragmentId = u32;
 pub type SpanId = u32;
-pub type StepId = usize;
+pub type NodeId = usize;
 
 pub const NULL_NODE: usize = 0;
+
+impl PathwayGraph {
+    pub fn new() -> Self {
+        // let null_node = Node::Step { step: Step::Max, child: NULL_NODE, parent: NULL_NODE };
+        // PathwayGraph { nodes: vec![null_node] }
+        PathwayGraph {
+            nodes: vec![],
+            edges: BTreeSet::new(),
+        }
+    }
+
+    pub fn node(&mut self, step: Step, children: Vec<NodeId>) -> NodeId {
+        let idx = self.nodes.len();
+        self.nodes.push(step);
+        for child in children {
+            self.add_edge(idx, child);
+        }
+        idx
+    }
+
+    fn add_edge(&mut self, parent: NodeId, child: NodeId) {
+        self.edges.insert(Edge::Forward {
+            a: parent,
+            b: child,
+        });
+        self.edges.insert(Edge::Backward {
+            b: child,
+            a: parent,
+        });
+    }
+
+    pub fn siblings(&self, node: NodeId) -> impl Iterator<Item=NodeId> + '_ {
+        self.parents(node)
+            .flat_map(move |parent| self.children(parent))
+            .filter(move |&child| child != node)
+    }
+
+    pub fn parents(&self, child: NodeId) -> impl Iterator<Item=NodeId> + '_ {
+        let parent_edge_a = Edge::Backward { b: child, a: 0 };
+        let parent_edge_b = Edge::Backward { b: child, a: !0 };
+        self.edges.range(parent_edge_a .. parent_edge_b)
+            .map(|&edge| {
+                if let Edge::Backward { a, .. } = edge { a } else { unreachable!() }
+            })
+    }
+
+    pub fn children(&self, parent: NodeId) -> impl Iterator<Item=NodeId> + '_ {
+        let sibling_edge_a = Edge::Forward { a: parent, b: 0 };
+        let sibling_edge_b = Edge::Forward { a: parent, b: !0 };
+        self.edges.range(sibling_edge_a .. sibling_edge_b)
+            .map(|&edge| {
+                if let Edge::Forward { b, .. } = edge { b } else { unreachable!() }
+            })
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=&Step> + '_ {
+        self.nodes.iter()
+    }
+
+    pub fn get(&self, step_id: NodeId) -> Option<Step> {
+        self.nodes.get(step_id).cloned()
+    }
+
+    pub fn roots(&self) -> impl Iterator<Item=NodeId> {
+        let mut is_root = vec![true; self.nodes.len()];
+        for &edge in &self.edges {
+            match edge {
+                Edge::Forward { a, b } => {
+                    is_root[b as usize] = false;
+                }
+                _ => {}
+            }
+        }
+        is_root.into_iter().enumerate().filter_map(|(id, root)| if root { Some(id) } else { None })
+    }
+
+    pub fn walk<'a>(&'a self) -> impl Iterator<Item=NodeWithChildren<'a>> {
+        self.iter().enumerate().map(|(i, &node)|
+            NodeWithChildren { id: i as NodeId, children: Box::new(self.children(i as NodeId)) as Box<dyn Iterator<Item=NodeId> + 'a> }
+        )
+    }
+}
+
+struct NodeWithChildren<'a> {
+    id: NodeId,
+    children: Box<dyn Iterator<Item = NodeId> + 'a>,
+}
--- panini_logic_salsa/src/lib.rs
+++ panini_logic_salsa/src/lib.rs
@@ -6,10 +6,56 @@ use elsa::FrozenIndexSet;
 use quote::quote;
 use proc_macro2::{TokenStream, Ident, Span};
 use indexmap::IndexSet;
+use itertools::Itertools;
+
+use graph::{PathwayGraph, FragmentId, Step, NodeId, BindId, IdxKind};
+
+extern crate salsa;
 
 mod graph;
 
-use itertools::Itertools;
+#[derive(Clone)]
+struct ComparableTokenStream(TokenStream);
+
+impl std::fmt::Debug for ComparableTokenStream {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl std::fmt::Display for ComparableTokenStream {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl PartialEq for ComparableTokenStream {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string().eq(&other.to_string())
+    }
+}
+
+impl Eq for ComparableTokenStream {
+    fn assert_receiver_is_total_eq(&self) {}
+}
+
+impl std::hash::Hash for ComparableTokenStream {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.to_string().hash(state)
+    }
+}
+
+impl From<TokenStream> for ComparableTokenStream {
+    fn from(token_stream: TokenStream) -> Self {
+        ComparableTokenStream(token_stream)
+    }
+}
+
+impl From<ComparableTokenStream> for TokenStream {
+    fn from(token_stream: ComparableTokenStream) -> Self {
+        token_stream.0
+    }
+}
 
 #[salsa::query_group(ProvideInputStorage)]
 trait ProvideInput {
@@ -20,12 +66,112 @@ trait ProvideInput {
 
     fn pretty_input(&self) -> String;
 
-    // TODO: Functional pretty print.
+    fn tokenize_input(&self) -> ComparableTokenStream;
 
+    fn tokenize_node(&self, node_id: NodeId) -> ComparableTokenStream;
+
+    fn tokenize_each_child(&self, node_id: NodeId) -> Vec<ComparableTokenStream>;
+
+    fn need_parentheses_around_children(&self, node_id: NodeId) -> bool;
+
+    fn get_node(&self, node_id: NodeId) -> Option<Step>;
+
+    fn get_sym(&self, fragment_id: FragmentId) -> String;
+
+    fn tokenize_children(&self, node_id: NodeId, separator: Option<ComparableTokenStream>, may_add_parentheses: bool) -> ComparableTokenStream;
+
+    fn get_bind(&self, bind_id: BindId) -> String;
+}
+
+fn get_node(db: &dyn ProvideInput, node_id: NodeId) -> Option<Step> {
+    db.input().graph.get(node_id)
+}
+
+fn get_bind(db: &dyn ProvideInput, bind_id: BindId) -> String {
+    let input = db.input();
+    let mut nth = input.bind_set.iter().nth(bind_id as usize);
+    nth.as_ref().expect("incorrect id in Bind { bind_id }").to_string()
+}
+
+fn get_sym(db: &dyn ProvideInput, fragment_id: FragmentId) -> String {
+    let input = db.input();
+    let mut nth = input.lhs_set.iter().nth(fragment_id as usize);
+    nth.as_ref().expect("incorrect id in Fragment(fragment_id)").to_string()
+}
+
+fn tokenize_each_child(db: &dyn ProvideInput, node_id: NodeId) -> Vec<ComparableTokenStream> {
+    db.input().graph.children(node_id).map(|child_id| db.tokenize_node(child_id)).collect()
+}
+
+fn need_parentheses_around_children(db: &dyn ProvideInput, node_id: NodeId) -> bool {
+    db.input().graph.children(node_id).enumerate().any(|(i, id)| {
+        match (i, db.input().graph.get(id).unwrap()) {
+            (0, Step::Fragment(..)) => false,
+            (0, Step::Sequence { .. }) => false,
+            _ => true,
+        }
+    })
+}
+
+fn tokenize_children(db: &dyn ProvideInput, node_id: NodeId, separator: Option<ComparableTokenStream>, may_add_parentheses: bool) -> ComparableTokenStream {
+    let mut inner = TokenStream::new();
+    if let Some(separator) = separator {
+        inner.extend(db.tokenize_each_child(node_id).into_iter().intersperse(separator).map(|s| s.0));
+    } else {
+        inner.extend(db.tokenize_each_child(node_id).into_iter().map(|s| s.0));
+    }
+    let add_parentheses = db.need_parentheses_around_children(node_id) && may_add_parentheses;
+    if add_parentheses {
+        quote! { ( #inner ) }.into()
+    } else {
+        inner.into()
+    }
+}
+
+fn tokenize_node(db: &dyn ProvideInput, node_id: NodeId) -> ComparableTokenStream {
+    match db.get_node(node_id).expect("invalid node_id") {
+        Step::Fragment(fragment_id) => {
+            let ident = db.get_sym(fragment_id);
+            let mut result = TokenStream::new();
+            result.extend(vec![TokenTree::Ident(Ident::new(&ident[..], Span::call_site()))]);
+            result
+        }
+        Step::Sequence { min: 0, max: None } => {
+            let children: TokenStream = db.tokenize_children(node_id, None, true).into();
+            quote! { #children * }
+        }
+        Step::Idx(IdxKind::Sum, _idx) => {
+            db.tokenize_children(node_id, Some(quote! { | }.into()), false).into()
+        }
+        Step::Idx(IdxKind::Product, _idx) => {
+            db.tokenize_children(node_id, None, false).into()
+        }
+        Step::Bind { bind_id, idx: _ } => {
+            let bind = db.get_bind(bind_id);
+            let bind_ident = TokenTree::Ident(Ident::new(&bind[..], Span::call_site()));
+            let children: TokenStream = db.tokenize_children(node_id, None, true).into();
+            quote! { #bind_ident : #children }
+        }
+        Step::StmtFragment(fragment_id) => {
+            let lhs_ident = db.get_sym(fragment_id);
+            let lhs = TokenTree::Ident(Ident::new(&lhs_ident[..], Span::call_site()));
+            let rhs: TokenStream = db.tokenize_children(node_id, None, false).into();
+            quote! { #lhs ::= #rhs ; }
+        }
+        other => panic!("UNEXPECTED STEP: {:?}", other),
+    }.into()
+}
+
+fn tokenize_input(db: &dyn ProvideInput) -> ComparableTokenStream {
+    let mut result = TokenStream::new();
+    for root_id in db.input().graph.roots() {
+        result.extend(db.tokenize_node(root_id).0);
+    }
+    result.into()
 }
 
 fn pretty_input(db: &dyn ProvideInput) -> String {
-    db.input().reduce().to_string()
+    db.tokenize_input().to_string()
 }
 
 #[salsa::database(ProvideInputStorage)]
@@ -35,19 +181,6 @@ struct DatabaseStruct {
 }
 
 impl salsa::Database for DatabaseStruct {}
-
-pub type IdentId = u32;
-pub type ExprId = u32;
-pub type AttrId = u32;
-pub type LexerId = u32;
-pub type TyId = u32;
-pub type BindId = u32;
-pub type FragmentId = u32;
-pub type SpanId = u32;
-
-pub const NULL_NODE: usize = 0;
-
-pub type StepId = usize;
 
 #[derive(Clone)]
 pub struct Input {
@@ -91,213 +224,6 @@ impl ::std::fmt::Debug for Input {
             bind_set: interner_to_vec(&self.bind_set),
             rule_indices: &self.rule_indices,
         }.fmt(fmt)
-    }
-}
-
-impl Input {
-    fn reduce(&self) -> TokenStream {
-        let more_than_only_one_fragment_or_sequence = |(i, id)| {
-            match (i, self.graph.get(id).unwrap()) {
-                (0, Step::Fragment(..)) => false,
-                (0, Step::Sequence { .. }) => false,
-                _ => true,
-            }
-        };
-        let mut content = vec![None; self.graph.nodes.len()];
-        for no_outgoing in self.graph.walk() {
-            let token_stream = match self.graph.get(no_outgoing.id) {
-                Some(Step::Fragment(fragment_id)) => {
-                    let mut stream = TokenStream::new();
-                    let mut nth = self.lhs_set.iter().nth(fragment_id as usize);
-                    let ident = nth.as_ref().expect("incorrect id in Fragment(fragment_id)");
-                    stream.extend(vec![TokenTree::Ident(Ident::new(ident, Span::call_site()))]);
-                    stream
-                }
-                Some(Step::Sequence { min: 0, max: None }) => {
-                    let mut stream = TokenStream::new();
-                    let children: Vec<_> = no_outgoing.children.collect();
-                    stream.extend(children.iter().cloned().map(|id| content[id as usize].clone().unwrap()));
-                    let add_parentheses = children.into_iter().enumerate().any(more_than_only_one_fragment_or_sequence);
-                    if add_parentheses {
-                        quote! { ( #stream ) * }
-                    } else {
-                        quote! { #stream * }
-                    }
-                }
-                Some(Step::Idx(IdxKind::Sum, _idx)) => {
-                    let mut stream = TokenStream::new();
-                    let alts = no_outgoing.children.map(|id| content[id as usize].clone().unwrap());
-                    let pipe = quote! { | };
-                    stream.extend(alts.intersperse(pipe));
-                    stream
-                }
-                Some(Step::Idx(IdxKind::Product, _idx)) => {
-                    let mut stream = TokenStream::new();
-                    let children: Vec<_> = no_outgoing.children.collect();
-                    stream.extend(children.iter().cloned().map(|id| content[id as usize].clone().unwrap()));
-                    stream
-                }
-                Some(Step::Bind { bind_id, idx: _ }) => {
-                    let mut nth = self.bind_set.iter().nth(bind_id as usize);
-                    let ident = nth.as_ref().expect("incorrect id in Bind { bind_id }");
-                    let ident = TokenTree::Ident(Ident::new(ident, Span::call_site()));
-                    let mut rhs = TokenStream::new();
-                    let children: Vec<_> = no_outgoing.children.collect();
-                    rhs.extend(children.iter().map(|&id| content[id as usize].clone().unwrap()));
-                    let add_parentheses = children.into_iter().enumerate().any(more_than_only_one_fragment_or_sequence);
-                    if add_parentheses {
-                        quote! { #ident : ( #rhs ) }
-                    } else {
-                        quote! { #ident : #rhs }
-                    }
-                }
-                Some(Step::StmtFragment(fragment_id)) => {
-                    let mut nth = self.lhs_set.iter().nth(fragment_id as usize);
-                    let ident = nth.as_ref().expect("incorrect id in StmtFragment(fragment_id)");
-                    let lhs = TokenTree::Ident(Ident::new(ident, Span::call_site()));
-                    let mut rhs = TokenStream::new();
-                    rhs.extend(no_outgoing.children.map(|id| content[id as usize].clone().unwrap()));
-                    quote! { #lhs ::= #rhs ; }
-                }
-                Some(Step::Sequence { min: _, max: _ }) => panic!(),
-                Some(other) => panic!("UNEXPECTED STEP: {:?}", other),
-                None => panic!("err")
-            };
-            eprintln!("{:?}", token_stream.to_string());
-            content[no_outgoing.id as usize] = Some(token_stream);
-        }
-        let mut result = TokenStream::new();
-        for root_id in self.graph.roots() {
-            result.extend(content.get(root_id).cloned().expect("err"));
-        }
-        result
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct PathwayGraph {
-    nodes: Vec<Step>,
-    edges: BTreeSet<Edge>,
-}
-
-#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
-enum Edge {
-    Forward {
-        a: usize,
-        b: usize,
-    },
-    Backward {
-        b: usize,
-        a: usize,
-    },
-}
-
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub enum Step {
-    Idx(IdxKind, usize),
-    Fragment(FragmentId),
-    StmtFragment(FragmentId),
-    StmtTy(TyId),
-    // Class(Class, Symbol),
-    // RightQuote,
-    Bind { bind_id: BindId, idx: usize },
-    Sequence { min: u32, max: Option<u32> },
-    SequenceEnd,
-    SequenceToken,
-    Max,
-}
-
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub enum IdxKind {
-    Sum,
-    Product,
-    Stmt,
-}
-
-struct NodeWithChildren<'a> {
-    id: StepId,
-    children: Box<dyn Iterator<Item = StepId> + 'a>,
-}
-
-impl PathwayGraph {
-    pub fn new() -> Self {
-        // let null_node = Node::Step { step: Step::Max, child: NULL_NODE, parent: NULL_NODE };
-        // PathwayGraph { nodes: vec![null_node] }
-        PathwayGraph {
-            nodes: vec![],
-            edges: BTreeSet::new(),
-        }
-    }
-
-    pub fn node(&mut self, step: Step, children: Vec<StepId>) -> StepId {
-        let idx = self.nodes.len();
-        self.nodes.push(step);
-        for child in children {
-            self.add_edge(idx, child);
-        }
-        idx
-    }
-
-    fn add_edge(&mut self, parent: StepId, child: StepId) {
-        self.edges.insert(Edge::Forward {
-            a: parent,
-            b: child,
-        });
-        self.edges.insert(Edge::Backward {
-            b: child,
-            a: parent,
-        });
-    }
-
-    pub fn siblings(&self, node: StepId) -> impl Iterator<Item=StepId> + '_ {
-        self.parents(node)
-            .flat_map(move |parent| self.children(parent))
-            .filter(move |&child| child != node)
-    }
-
-    pub fn parents(&self, child: StepId) -> impl Iterator<Item=StepId> + '_ {
-        let parent_edge_a = Edge::Backward { b: child, a: 0 };
-        let parent_edge_b = Edge::Backward { b: child, a: !0 };
-        self.edges.range(parent_edge_a .. parent_edge_b)
-            .map(|&edge| {
-                if let Edge::Backward { a, .. } = edge { a } else { unreachable!() }
-            })
-    }
-
-    pub fn children(&self, parent: StepId) -> impl Iterator<Item=StepId> + '_ {
-        let sibling_edge_a = Edge::Forward { a: parent, b: 0 };
-        let sibling_edge_b = Edge::Forward { a: parent, b: !0 };
-        self.edges.range(sibling_edge_a .. sibling_edge_b)
-            .map(|&edge| {
-                if let Edge::Forward { b, .. } = edge { b } else { unreachable!() }
-            })
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item=&Step> + '_ {
-        self.nodes.iter()
-    }
-
-    pub fn get(&self, step_id: StepId) -> Option<Step> {
-        self.nodes.get(step_id).cloned()
-    }
-
-    pub fn roots(&self) -> impl Iterator<Item=StepId> {
-        let mut is_root = vec![true; self.nodes.len()];
-        for &edge in &self.edges {
-            match edge {
-                Edge::Forward { a, b } => {
-                    is_root[b as usize] = false;
-                }
-                _ => {}
-            }
-        }
-        is_root.into_iter().enumerate().filter_map(|(id, root)| if root { Some(id) } else { None })
-    }
-
-    pub fn walk<'a>(&'a self) -> impl Iterator<Item=NodeWithChildren<'a>> {
-        self.iter().enumerate().map(|(i, &node)|
-            NodeWithChildren { id: i as StepId, children: Box::new(self.children(i as StepId)) as Box<dyn Iterator<Item=StepId> + 'a> }
-        )
     }
 }
 
@@ -520,14 +446,17 @@ mod tests {
                     start@0 b@1;
                 }
                 trace == {
-                    start@0 a@0 => a;
-                    start@0 b@1 => b; 
+                    start@0 a.0 => a;
+                    start@0 b.1 => b; 
                 }
                 rewritten == {
                     start ::= 0 a 1 b 2;
                 }
                 types == {
                     start: (typeof a, typeof b);
+                }
+                program == {
+
                 }
             }
         );
@@ -549,42 +478,42 @@ mod tests {
                 }
                 trace == {
                     // 0
-                    start@0 * * => LParen;
+                    start .0 * * => LParen;
                     // 1
-                    start@0 * * a@0 => a;
+                    start .0 * * .0 => a;
                     // 2
-                    start@0 * * b@1 => b;
+                    start .0 * * .1 => b;
                     // 3
-                    start@0 * * SequenceEnd => RParen;
+                    start .0 * * "SequenceEnd" => RParen;
                     // 4
-                    start@0 * * SequenceToken => *;
+                    start .0 * * "SequenceToken" => *;
                     // 5
-                    start@0 * SequenceToken => *;
+                    start .0 * "SequenceToken" => *;
                     // 6
                 }
                 rewritten == {
-                    start@0 * => {
+                    start .0 * => {
                         start ::= 0 generated[3]* 6;
-                    }
-                    start@0 * * => {
-                        generated[3] ::= (1 a 2 b 3)* 5;
+                        * => {
+                            generated[3] ::= (1 a 2 b 3)* 5;
+                        }
                     }
                 }
                 types == {
-                    start@0: {
+                    start .0: {
                         * * a: [[typeof a]],
-                        * * b: [[typeof b]]
+                        * * b: [[typeof b]],
                     };
-                    start@0 *: {
+                    start .0 *: {
                         * a: [typeof a],
                         * b: [typeof b]
                     };
-                    start@0 * *: {
+                    start .0 * *: {
                         a: typeof a,
                         b: typeof b
                     };
-                    start@0 * * a: typeof a;
-                    start@0 * * b: typeof b;
+                    start .0 * * a: typeof a;
+                    start .0 * * b: typeof b;
                 }
             }
         );
